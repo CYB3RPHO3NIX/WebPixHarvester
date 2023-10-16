2a95ca79426ffd23f60dc22b153fc989266c5d58
DHEERAJ KUMAR <98643857+CYB3RPHO3NIX@users.noreply.github.com>
2023-10-16 22:04:18 +0530

    working code

--- WebPixHarvester.UserInterface/MainWindow.Designer.cs
+++ WebPixHarvester.UserInterface/MainWindow.Designer.cs
@@ -40,14 +40,19 @@
             textBox1 = new TextBox();
             label2 = new Label();
             textBox2 = new TextBox();
+            label3 = new Label();
+            trackBar1 = new TrackBar();
             button1 = new Button();
             button2 = new Button();
             button3 = new Button();
             lowerPanel = new Panel();
+            flowLayoutPanel = new FlowLayoutPanel();
             statusStrip1.SuspendLayout();
             upperPanel.SuspendLayout();
             groupBox1.SuspendLayout();
             flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            lowerPanel.SuspendLayout();
             SuspendLayout();
             // 
             // statusStrip1
@@ -55,7 +60,7 @@
             statusStrip1.Items.AddRange(new ToolStripItem[] { totalURLs, totalImages, toolStripProgressBar1 });
             statusStrip1.Location = new Point(0, 439);
             statusStrip1.Name = "statusStrip1";
-            statusStrip1.Size = new Size(784, 22);
+            statusStrip1.Size = new Size(984, 22);
             statusStrip1.TabIndex = 0;
             statusStrip1.Text = "statusStrip1";
             // 
@@ -86,7 +91,7 @@
             upperPanel.Location = new Point(0, 0);
             upperPanel.Name = "upperPanel";
             upperPanel.Padding = new Padding(8);
-            upperPanel.Size = new Size(784, 70);
+            upperPanel.Size = new Size(984, 68);
             upperPanel.TabIndex = 1;
             // 
             // groupBox1
@@ -95,7 +100,7 @@
             groupBox1.Dock = DockStyle.Fill;
             groupBox1.Location = new Point(8, 8);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(768, 54);
+            groupBox1.Size = new Size(968, 52);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Controls";
@@ -107,13 +112,15 @@
             flowLayoutPanel1.Controls.Add(textBox1);
             flowLayoutPanel1.Controls.Add(label2);
             flowLayoutPanel1.Controls.Add(textBox2);
+            flowLayoutPanel1.Controls.Add(label3);
+            flowLayoutPanel1.Controls.Add(trackBar1);
             flowLayoutPanel1.Controls.Add(button1);
             flowLayoutPanel1.Controls.Add(button2);
             flowLayoutPanel1.Controls.Add(button3);
             flowLayoutPanel1.Dock = DockStyle.Fill;
             flowLayoutPanel1.Location = new Point(3, 19);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(762, 32);
+            flowLayoutPanel1.Size = new Size(962, 30);
             flowLayoutPanel1.TabIndex = 0;
             // 
             // button4
@@ -162,9 +169,33 @@
             textBox2.Size = new Size(100, 23);
             textBox2.TabIndex = 4;
             // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            label3.AutoSize = true;
+            label3.Location = new Point(437, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(87, 29);
+            label3.TabIndex = 9;
+            label3.Text = "Thumbnail Size";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // trackBar1
+            // 
+            trackBar1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            trackBar1.Location = new Point(530, 3);
+            trackBar1.Maximum = 500;
+            trackBar1.Minimum = 100;
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(104, 23);
+            trackBar1.SmallChange = 50;
+            trackBar1.TabIndex = 10;
+            trackBar1.Value = 100;
+            trackBar1.Scroll += trackBar1_Scroll;
+            // 
             // button1
             // 
-            button1.Location = new Point(437, 3);
+            button1.Location = new Point(640, 3);
             button1.Name = "button1";
             button1.Size = new Size(100, 23);
             button1.TabIndex = 5;
@@ -174,7 +205,7 @@
             // 
             // button2
             // 
-            button2.Location = new Point(543, 3);
+            button2.Location = new Point(746, 3);
             button2.Name = "button2";
             button2.Size = new Size(84, 23);
             button2.TabIndex = 6;
@@ -183,7 +214,7 @@
             // 
             // button3
             // 
-            button3.Location = new Point(633, 3);
+            button3.Location = new Point(836, 3);
             button3.Name = "button3";
             button3.Size = new Size(118, 23);
             button3.TabIndex = 7;
@@ -192,21 +223,31 @@
             // 
             // lowerPanel
             // 
+            lowerPanel.Controls.Add(flowLayoutPanel);
             lowerPanel.Dock = DockStyle.Fill;
-            lowerPanel.Location = new Point(0, 70);
+            lowerPanel.Location = new Point(0, 68);
             lowerPanel.Name = "lowerPanel";
-            lowerPanel.Size = new Size(784, 369);
+            lowerPanel.Size = new Size(984, 371);
             lowerPanel.TabIndex = 2;
+            // 
+            // flowLayoutPanel
+            // 
+            flowLayoutPanel.AutoScroll = true;
+            flowLayoutPanel.Dock = DockStyle.Fill;
+            flowLayoutPanel.Location = new Point(0, 0);
+            flowLayoutPanel.Name = "flowLayoutPanel";
+            flowLayoutPanel.Size = new Size(984, 371);
+            flowLayoutPanel.TabIndex = 0;
             // 
             // MainWindow
             // 
             AutoScaleDimensions = new SizeF(96F, 96F);
             AutoScaleMode = AutoScaleMode.Dpi;
-            ClientSize = new Size(784, 461);
+            ClientSize = new Size(984, 461);
             Controls.Add(lowerPanel);
             Controls.Add(upperPanel);
             Controls.Add(statusStrip1);
-            MinimumSize = new Size(800, 500);
+            MinimumSize = new Size(1000, 500);
             Name = "MainWindow";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "WebPix Harvester";
@@ -216,6 +257,8 @@
             groupBox1.ResumeLayout(false);
             flowLayoutPanel1.ResumeLayout(false);
             flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            lowerPanel.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -238,5 +281,8 @@
         private Button button3;
         private Button button4;
         private Panel lowerPanel;
+        private FlowLayoutPanel flowLayoutPanel;
+        private Label label3;
+        private TrackBar trackBar1;
     }
 }
--- WebPixHarvester.UserInterface/MainWindow.cs
+++ WebPixHarvester.UserInterface/MainWindow.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
@@ -6,6 +7,7 @@ namespace WebPixHarvester.UserInterface
     public partial class MainWindow : Form
     {
         private Form _LinksLoader = null;
+        private List<string> _ImageList = null;
         public MainWindow()
         {
             InitializeComponent();
@@ -35,8 +37,50 @@ namespace WebPixHarvester.UserInterface
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            List<string> paths = new List<string>()
+            {
+                "C:\\Users\\CyberPhoenix\\Desktop\\Images\\IMG (1).png",
+                "C:\\Users\\CyberPhoenix\\Desktop\\Images\\IMG (2).png",
+                "C:\\Users\\CyberPhoenix\\Desktop\\Images\\IMG (3).png",
+                "C:\\Users\\CyberPhoenix\\Desktop\\Images\\IMG (4).png"
+            };
+            _ImageList = paths;
+            PopulateFlowLayoutPanel();
         }
-        
+        private Image GetThumbnail(string imageUrl, int thumbnailSize)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                byte[] data = wc.DownloadData(imageUrl);
+                using (MemoryStream ms = new MemoryStream(data))
+                {
+                    Image fullSizeImage = Image.FromStream(ms);
+                    return fullSizeImage.GetThumbnailImage(thumbnailSize, thumbnailSize, null, IntPtr.Zero);
+                }
+            }
+        }
+        private void PopulateFlowLayoutPanel(int thumbSize = 200)
+        {
+            flowLayoutPanel.Controls.Clear();
+            // Assuming you have a list of image URLs in imageURLsList
+            foreach (string imageUrl in _ImageList)
+            {
+
+                Image thumbnail = GetThumbnail(imageUrl, thumbSize);
+                PictureBox pictureBox = new PictureBox
+                {
+                    Image = thumbnail,
+                    SizeMode = PictureBoxSizeMode.Zoom, // Adjust this based on your preference
+                    Width = thumbSize,
+                    Height = thumbSize
+                };
+                flowLayoutPanel.Controls.Add(pictureBox);
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            PopulateFlowLayoutPanel(trackBar1.Value);
+        }
     }
 }
